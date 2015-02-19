cd85574e9a867c865896af07e93c3673373f7f33
Vivienne Fischer <git@vien.fischerfamily.us>
2015-02-19 11:03:54 -0800

    added list of scheduled requests

--- app/src/main/java/com/onemobilekidz/mobilekidz/Schedule.java
+++ app/src/main/java/com/onemobilekidz/mobilekidz/Schedule.java
@@ -1,39 +1,20 @@
 package com.onemobilekidz.mobilekidz;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
 
 
-public class Schedule extends ActionBarActivity {
+public class Schedule extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        final String[] scheduleArray = getResources().getStringArray(R.array.schedule);
+        ListView scheduleList = (ListView) findViewById(R.id.scheduleList);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_schedule, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
