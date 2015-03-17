f77dcd5b721a2515c16456d6f6a664f3f156b193
Vivienne Fischer <git@vien.fischerfamily.us>
2015-03-17 16:00:43 -0700

    added a link in Make Requests page for user to be able to view points and created a points page

--- app/src/main/java/com/onemobilekidz/mobilekidz/MakeRequests.java
+++ app/src/main/java/com/onemobilekidz/mobilekidz/MakeRequests.java
@@ -179,27 +179,12 @@ public class MakeRequests extends Activity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_make_requests, menu);
-        return true;
+    public void viewPoints(View view) {
+        Intent intent = new Intent(this, Points.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 }
--- app/src/main/java/com/onemobilekidz/mobilekidz/Points.java
+++ app/src/main/java/com/onemobilekidz/mobilekidz/Points.java
@@ -1,5 +1,6 @@
 package com.onemobilekidz.mobilekidz;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +11,7 @@ import com.onemobilekidz.mobilekidz.helper.DatabaseManager;
 import com.onemobilekidz.mobilekidz.model.PointsModel;
 
 
-public class Points extends ActionBarActivity {
+public class Points extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
