3941250a9cb8008cafd34c325aa532932a2ac746
Vivienne Fischer <git@vien.fischerfamily.us>
2015-03-17 15:48:32 -0700

    added points page

---
+++ app/src/main/java/com/onemobilekidz/mobilekidz/Points.java
@@ -0,0 +1,30 @@
+package com.onemobilekidz.mobilekidz;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.onemobilekidz.mobilekidz.helper.DatabaseManager;
+import com.onemobilekidz.mobilekidz.model.PointsModel;
+
+
+public class Points extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points);
+    }
+
+    public void assignPoints(View view) {
+
+        DatabaseManager dbManager = new DatabaseManager(this);
+        PointsModel pointsObj = new PointsModel(5, 2);
+        dbManager.addRowPoints(pointsObj);
+
+    }
+
+
+}
