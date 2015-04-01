9a77f4f112b8351c207fce740bd3bde67d2d62ff
Vivienne Fischer <git@vien.fischerfamily.us>
2015-03-31 23:01:45 -0700

    display the schedule

--- app/src/main/java/com/onemobilekidz/mobilekidz/Schedule.java
+++ app/src/main/java/com/onemobilekidz/mobilekidz/Schedule.java
@@ -1,28 +1,94 @@
 package com.onemobilekidz.mobilekidz;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.onemobilekidz.mobilekidz.model.UserModel;
 
 
-public class Schedule extends Activity {
+public class Schedule extends ListActivity {
+
+    private static final String LOG = "Schedule";
+
+    private static final String FIREBASE_URL = "https://crackling-heat-9656.firebaseio.com/";
+    private Firebase mFirebaseRef;
+    private ValueEventListener mConnectedListener;
+    private ScheduleListAdapter scheduleListAdapter;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        final String[] scheduleArray = getResources().getStringArray(R.array.schedule);
-        ListView scheduleList = (ListView) findViewById(R.id.scheduleList);
+        Log.v(LOG, "this is my userid " + UserModel.getCurrentUser().getUserId());
+        // Setup our Firebase mFirebaseRef
+        try {
+            mFirebaseRef = new Firebase(FIREBASE_URL).child("my_schedule").child(UserModel.getCurrentUser().getUserId());
+        } catch (Exception e) {
+            Log.e(LOG, e.toString());
+        }
     }
 
 
-    public void onMySchedule(View view) {
-        Intent intent = new Intent(this, Schedule.class);
-        startActivity(intent);
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = getListView();
+
+        if (mFirebaseRef != null) {
+            scheduleListAdapter = new ScheduleListAdapter(getApplicationContext(), mFirebaseRef, this, R.layout.out_babysitting_request_list_row);
+            Log.v(LOG, "getCount: " + scheduleListAdapter.getCount());
+
+            listView.setAdapter(scheduleListAdapter);
+            scheduleListAdapter.registerDataSetObserver(new DataSetObserver() {
+                @Override
+                public void onChanged() {
+                    super.onChanged();
+                    listView.setSelection(scheduleListAdapter.getCount() - 1);
+                }
+            });
+
+// Finally, a little indication of connection status
+            mConnectedListener = mFirebaseRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    boolean connected = (Boolean) dataSnapshot.getValue();
+                    if (connected) {
+                        Toast.makeText(Schedule.this, "Connected to Firebase", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(Schedule.this, "Disconnected from Firebase", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+// No-op
+                }
+            });
+        }
 
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mFirebaseRef != null) {
+            mFirebaseRef.getRoot().child(".info/connected").removeEventListener(mConnectedListener);
+            scheduleListAdapter.cleanup();
+        }
+    }
 
 }
---
+++ app/src/main/java/com/onemobilekidz/mobilekidz/ScheduleListAdapter.java
@@ -0,0 +1,73 @@
+package com.onemobilekidz.mobilekidz;
+
+
+/**
+ * Created by vfischer on 3/31/15.
+ */
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.client.Query;
+import com.onemobilekidz.mobilekidz.model.InBabysittingRequestsModel;
+import com.onemobilekidz.mobilekidz.model.OutBabysittingRequestsModel;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by vfischer on 3/31/15.
+ */
+
+public class ScheduleListAdapter extends FirebaseListAdapter<InBabysittingRequestsModel> {
+
+    private static final String LOG = "SchedListAdapter";
+    private static final String FIREBASE_URL = "https://crackling-heat-9656.firebaseio.com/";
+
+    public ScheduleListAdapter(Context context, Query ref, Activity activity, int layout) {
+        super(context, ref, InBabysittingRequestsModel.class, layout, activity);
+        Log.v(LOG, "I'm here");
+    }
+
+    /**
+     * Bind an instance of the <code>FriendRequestsModel</code> class to our view. This method is called by <code>FirebaseListAdapter</code>
+     * when there is a data change, and we are given an instance of a View that corresponds to the layout that we passed
+     * to the constructor, as well as a single <code>FriendRequestsModel</code> instance that represents the current data to bind.
+     *
+     * @param view                  A view instance corresponding to the layout we passed to the constructor.
+     * @param babysittingRequestObj An instance representing the current state of a chat message
+     */
+
+
+    @Override
+    protected void populateView(View view, final InBabysittingRequestsModel babysittingRequestObj, final int i) {
+        final String requestor;
+        final String requestorName;
+        final String jobStartTime;
+        final String jobEndTime;
+        Log.v(LOG, "I'm here still");
+        if (babysittingRequestObj != null) {
+            requestor = babysittingRequestObj.getRequestor();
+            requestorName = babysittingRequestObj.getRequestorName();
+            jobStartTime = babysittingRequestObj.getJob_start_time();
+            jobEndTime = babysittingRequestObj.getJob_end_time();
+            final TextView babysitterNameText = (TextView) view.findViewById(R.id.babySitterName);
+            babysitterNameText.setText(requestorName);
+
+            final TextView jobStartTimeText = (TextView) view.findViewById(R.id.requestDate);
+            jobStartTimeText.setText(jobStartTime);
+
+            final TextView jobEndTimeText = (TextView) view.findViewById(R.id.requestEndTime);
+            jobEndTimeText.setText(jobEndTime);
+
+            Log.v(LOG, "requestor: " + requestor);
+            Log.v(LOG, " requestorName: " + requestorName + " jobstartTime: " + jobStartTime + " jobEndTime: " + jobEndTime);
+
+
+        }
+    }
+
+}
