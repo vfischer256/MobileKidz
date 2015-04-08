e2969cb06115aa717cb7f42654aac728d5fa3709
Vivienne Fischer <git@vien.fischerfamily.us>
2015-04-08 11:58:13 -0700

    added tests

---
+++ app/src/androidTest/java/com/onemobilekidz/mobilekidz/UIHomeTest.java
@@ -0,0 +1,20 @@
+package com.onemobilekidz.mobilekidz;
+
+import android.support.test.uiautomator.Until;
+import android.test.InstrumentationTestCase;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.By;
+
+public class UIHomeTest extends InstrumentationTestCase {
+
+    private UiDevice mDevice;
+
+    public void setUp() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+    }
+}
