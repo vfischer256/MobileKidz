20094b02f6af60fb60ca5de816e579106e163469
Vivienne Fischer <git@vien.fischerfamily.us>
2015-04-24 17:51:49 -0700

    added email validator

--- app/src/main/java/com/onemobilekidz/mobilekidz/FriendRequests.java
+++ app/src/main/java/com/onemobilekidz/mobilekidz/FriendRequests.java
@@ -15,6 +15,8 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 import com.onemobilekidz.mobilekidz.model.UserModel;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 import java.util.Map;
 
 
@@ -88,31 +90,44 @@ public class FriendRequests extends ListActivity {
         }
     }
 
+    private Boolean emailValidator(String email) {
+        EmailValidator emailValidator = EmailValidator.getInstance(true);
+        if (emailValidator.isValid(email)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // TODO: When we send a friend request to a user that doesn't exist, it makes a bogus user.
     // TODO: Delete the bogus user after 60 days if the user does not log in at least once.
     public void sendFriendRequest(View view) {
         final EditText editText = (EditText) findViewById(R.id.editEmailAddress);
         final String email = editText.getText().toString().toLowerCase();
-        System.out.println("Sending friend request from " + UserModel.getCurrentUser().getEmail() + " to " + email);
-        UserModel.getOrCreateUserByEmail(email, new ValueEventListener() {
-            public void onDataChange(DataSnapshot snapshot) {
-                String friendId = null;
-                for (String i : ((Map<String, Object>) snapshot.getValue()).keySet()) {
-                    friendId = i;
+        if (emailValidator(email)) {
+            System.out.println("Sending friend request from " + UserModel.getCurrentUser().getEmail() + " to " + email);
+            UserModel.getOrCreateUserByEmail(email, new ValueEventListener() {
+                public void onDataChange(DataSnapshot snapshot) {
+                    String friendId = null;
+                    for (String i : ((Map<String, Object>) snapshot.getValue()).keySet()) {
+                        friendId = i;
+                    }
+                    System.out.println("Sending friend request from " + UserModel.getCurrentUser().getUserId() + " to " + email + " (" + friendId + ")");
+                    new Firebase(FIREBASE_URL).child("friend_requests").
+                            child(friendId).child(UserModel.getCurrentUser().getUserId()).child("sender").setValue(true);
+                    Toast.makeText(FriendRequests.this, "Friend Request Sent ", Toast.LENGTH_SHORT).show();
+
+
+                    editText.setText("");
+                    editText.setHint("Enter email address");
                 }
-                System.out.println("Sending friend request from " + UserModel.getCurrentUser().getUserId() + " to " + email + " (" + friendId + ")");
-                new Firebase(FIREBASE_URL).child("friend_requests").
-                        child(friendId).child(UserModel.getCurrentUser().getUserId()).child("sender").setValue(true);
-                Toast.makeText(FriendRequests.this, "Friend Request Sent ", Toast.LENGTH_SHORT).show();
 
-
-                editText.setText("");
-                editText.setHint("Enter email address");
-            }
-
-            public void onCancelled(FirebaseError firebaseError) {
-            }
-        });
+                public void onCancelled(FirebaseError firebaseError) {
+                }
+            });
+        } else {
+            Toast.makeText(FriendRequests.this, "Invalid Email Address", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
