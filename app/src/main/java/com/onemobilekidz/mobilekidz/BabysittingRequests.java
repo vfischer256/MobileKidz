b574f39347b9ac2072b76452a013c5d57fb4a1cc
vfischer <vfischer484+git@gmail.com>
2015-06-25 15:00:24 -0700

    added logging

--- app/src/main/java/com/onemobilekidz/mobilekidz/BabysittingRequests.java
+++ app/src/main/java/com/onemobilekidz/mobilekidz/BabysittingRequests.java
@@ -16,21 +16,25 @@ public class BabysittingRequests extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_babysitting_requests);
+        Log.v(LOG, "Loading the activity");
 
     }
 
     public void goIncomingBabysittingRequests(View view) {
         intent = new Intent(this, InBabysittingRequests.class);
+        Log.v(LOG, "Loading the Incoming Babysitting Requests");
         startActivity(intent);
     }
 
     public void goOutgoingBabysittingRequests(View view) {
         intent = new Intent(this, OutBabysittingRequests.class);
+        Log.v(LOG, "Loading the Outgoing Babysitting Requests");
         startActivity(intent);
     }
 
     public void goMakeRequests(View view) {
         intent = new Intent(this, MakeRequests.class);
+        Log.v(LOG, "Loading the Make Requests");
         startActivity(intent);
     }
 }
